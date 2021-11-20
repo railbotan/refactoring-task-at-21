a8c4af3b953f7d4abff0f5e4bd8f138c3c62a45c
maksaevalisa <lisa.maksaeva@gmail.com>
2021-11-20 13:55:18 +0500

    Second step

--- filter.py
+++ filter.py
@@ -4,29 +4,39 @@ import numpy as np
 img = Image.open("img2.jpg")
 arr = np.array(img)
 
-a = len(arr)
-a1 = len(arr[1])
+length = len(arr)
+length_first = len(arr[1])
+size = int(input("Введите размер мозайки >> "))
+gray = int(input("Введите режим градации серого >> "))
 
 i = 0
-while i < a - 1:
+
+def sumColor():
+    global n, n1, sum_color
+    for n in range(i, i + size):
+        for n1 in range(j, j + size):
+            r = int(arr[n][n1][0])
+            g = int(arr[n][n1][1])
+            b = int(arr[n][n1][2])
+            sum_color += r + g + b
+    sum_color = int(sum_color // 100)
+
+def grayColor   ():
+    global n, n1
+    for n in range(i, i + size):
+        for n1 in range(j, j + size):
+            arr[n][n1][0] = int(sum_color // 50) * 50 // gray
+            arr[n][n1][1] = int(sum_color // 50) * 50 // gray
+            arr[n][n1][2] = int(sum_color // 50) * 50 // gray
+
+while i < length - 1:
     j = 0
-    while j < a1 - 1:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = int(arr[n][n1][0])
-                n2 = int(arr[n][n1][1])
-                n3 = int(arr[n][n1][2])
-                s += n1 + n2 + n3
-        s = int(s // 100)
-        
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s//3)
-                arr[n][n1][1] = int(s//3)
-                arr[n][n1][2] = int(s//3)
-        j = j + 10
-    i = i + 10
+    while j < length_first - 1:
+        sum_color = 0
+        sumColor()
+        grayColor()
+        j = j + size
+    i = i + size
     
 res = Image.fromarray(arr)
 res.show()
