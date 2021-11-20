4cb487134f301a2d1b45086d959fdc2fd95d7e59
two-sweaters <ivanpalichev@gmail.com>
2021-11-20 19:22:36 +0500

    третий этап is done

--- filter.py
+++ filter.py
@@ -3,31 +3,19 @@ import numpy as np
 
 img = Image.open("img2.jpg")
 pixels = np.array(img)
-width = len(pixels)
-height = len(pixels[1])
+height = len(pixels)
+width = len(pixels[1])
 mosaic_size = int(input("Размер мозаики: "))
 color_amount = int(input("Кол-во оттенков серого: "))
 
 
 def find_average(x, y):
-    s = 0
-    for n in range(x, min(x + mosaic_size, width)):
-        for m in range(y, min(y + mosaic_size, height)):
-            r = pixels[n][m][0]
-            g = pixels[n][m][1]
-            b = pixels[n][m][2]
-            brightness = r / 3 + g / 3 + b / 3
-            s += brightness
-    return int(s // mosaic_size ** 2)
+    return np.average(pixels[x:x + mosaic_size, y:y + mosaic_size])
 
 
 def change_pixels(x, y, average):
     gray_step = 255 // (color_amount - 1)
-    for n in range(x, min(x + mosaic_size, width)):
-        for m in range(y, min(y + mosaic_size, height)):
-            pixels[n][m][0] = int(average // gray_step) * gray_step
-            pixels[n][m][1] = int(average // gray_step) * gray_step
-            pixels[n][m][2] = int(average // gray_step) * gray_step
+    pixels[x:x + mosaic_size, y:y + mosaic_size] = int(average // gray_step) * gray_step
 
 
 def process_pixels():
