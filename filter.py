5ded97cb5de0a57b0e0e4c5072a03d4a8049d4cc
K-mixam <maxdzhigil@gmail.com>
2021-11-24 22:48:15 +0500

    second stage

--- filter.py
+++ filter.py
@@ -1,32 +1,34 @@
 from PIL import Image
 import numpy as np
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 1:
-    j = 0
-    
-    # если остаются необработанные 10 пикселей снизу и справа, то здесь должно быть "j < a1 - 1",
-    # а не "j < a1 - 11". Но тогда у меня возникает ошибка IndexError в цикле с итератором "j". Что странно, 
-    # потому что с итератором "i" таких проблем не возникает. Думаю, что проблема будет решена на этапе 
-    # замены циклов матричными преобразованиями
-    while j < a1 - 11:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                r = int(arr[n][n1][0])
-                g = int(arr[n][n1][1])
-                b = int(arr[n][n1][2])
-                s += r + g + b
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 3)
-                arr[n][n1][1] = int(s // 3)
-                arr[n][n1][2] = int(s // 3)
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
+rgb_table = np.array(img)
+height = len(rgb_table)
+width = len(rgb_table[1])
+mosaic_size = int(input("Enter the mosaic size "))
+grayscale_value = int(input("Enter the grayscale value "))
+
+
+def count_average_brightness(_i, _j):
+        average_brightness = 0
+        for n in range(_i, _i + mosaic_size):
+            for n1 in range(_j, _j + mosaic_size):
+                r = int(rgb_table[n][n1][0])
+                g = int(rgb_table[n][n1][1])
+                b = int(rgb_table[n][n1][2])
+                average_brightness += r//3 + g//3 + b//3
+        return int(average_brightness // (mosaic_size * mosaic_size))
+
+
+def applying_the_filter(_i, _j, _average_brightness):
+        for n in range(_i, _i + mosaic_size):
+            for n1 in range(_j, _j + mosaic_size):
+                rgb_table[n][n1][0] = int(_average_brightness // grayscale_value) * grayscale_value
+                rgb_table[n][n1][1] = int(_average_brightness // grayscale_value) * grayscale_value
+                rgb_table[n][n1][2] = int(_average_brightness // grayscale_value) * grayscale_value
+
+
+for i in range(0, height, mosaic_size):
+    for j in range(0, width, mosaic_size):
+        applying_the_filter(i, j, count_average_brightness(i, j))
+res = Image.fromarray(rgb_table)
 res.save('res.jpg')
