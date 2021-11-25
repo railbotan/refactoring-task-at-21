6cf35fca9ddf9045fe51cd32f7d2b15154921ed8
K-mixam <maxdzhigil@gmail.com>
2021-11-25 09:44:20 +0500

    third stage

--- filter.py
+++ filter.py
@@ -9,22 +9,12 @@ grayscale_value = int(input("Enter the grayscale value "))
 
 
 def count_average_brightness(_i, _j):
-        average_brightness = 0
-        for n in range(_i, _i + mosaic_size):
-            for n1 in range(_j, _j + mosaic_size):
-                r = int(rgb_table[n][n1][0])
-                g = int(rgb_table[n][n1][1])
-                b = int(rgb_table[n][n1][2])
-                average_brightness += r//3 + g//3 + b//3
-        return int(average_brightness // (mosaic_size * mosaic_size))
+        return np.average(rgb_table[_i: _i + mosaic_size, _j: _j + mosaic_size])
 
 
 def applying_the_filter(_i, _j, _average_brightness):
-        for n in range(_i, _i + mosaic_size):
-            for n1 in range(_j, _j + mosaic_size):
-                rgb_table[n][n1][0] = int(_average_brightness // grayscale_value) * grayscale_value
-                rgb_table[n][n1][1] = int(_average_brightness // grayscale_value) * grayscale_value
-                rgb_table[n][n1][2] = int(_average_brightness // grayscale_value) * grayscale_value
+        rgb_table[_i: _i + mosaic_size, _j: _j + mosaic_size] = int(_average_brightness // grayscale_value)\
+                                                                * grayscale_value
 
 
 for i in range(0, height, mosaic_size):
